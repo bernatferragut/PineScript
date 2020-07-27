7becd703a6fe2141df8f0b4cd77b3e18ba23d913
bernat <elbernat@gmail.com>
2020-07-27 11:03:15 -0400

    update

--- F&G/web/js/main.js
+++ F&G/web/js/main.js
@@ -2,22 +2,33 @@
 console.log("connected");
 
 // GET API DATA
-// Example URL: https://api.alternative.me/fng/
-// Example URL: https://api.alternative.me/fng/?limit=10
-// Example URL: https://api.alternative.me/fng/?limit=10&format=csv
-// Example URL: https://api.alternative.me/fng/?limit=10&format=csv&date_format=us
+// let entry = document.querySelector("#main");
+// let fearGreedUrl = "https://api.alternative.me/fng/?limit=10";
 
-let entry = document.querySelector("#main");
+// let xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//        // Typical action to be performed when the document is ready:
+//        console.log(xhttp.responseText);
+//        entry.innerHTML = xhttp.responseText;
+//     }
+// };
+// xhttp.open("GET", fearGreedUrl, true);
+// xhttp.send();
 
-let fearGreedUrl = "https://api.alternative.me/fng/?limit=10";
 
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       console.log(xhttp.responseText);
-       entry.innerHTML = xhttp.responseText;
-    }
-};
-xhttp.open("GET", fearGreedUrl, true);
-xhttp.send();
+// elements selection
+const row = document.getElementById("list");
+console.log(row);
+// function creation on the window level
+function run() {
+    const newText = document.createElement('p');
+    newText.textContent = 'Bernat';
+    row.appendChild(newText);
+    console.log(newText);
+}
+
+
+
+
+
