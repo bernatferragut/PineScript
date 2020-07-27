0f2363893c4ff8d6413e590ade722e492953b906
bernat <elbernat@gmail.com>
2020-07-27 14:12:46 -0400

    update

--- F&G/web/js/main.js
+++ F&G/web/js/main.js
@@ -1,34 +1,38 @@
 
 console.log("connected");
 
-// GET API DATA
-// let entry = document.querySelector("#main");
-// let fearGreedUrl = "https://api.alternative.me/fng/?limit=10";
-
-// let xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//        // Typical action to be performed when the document is ready:
-//        console.log(xhttp.responseText);
-//        entry.innerHTML = xhttp.responseText;
-//     }
-// };
-// xhttp.open("GET", fearGreedUrl, true);
-// xhttp.send();
-
-
 // elements selection
 const el = document.getElementById("rowList");
-console.log(el);
 // function creation on the window level
-function run() {
-    const data = document.createElement('p');
-    data.textContent = 'DATA';
-    el.appendChild(data);
-    console.log(data);
+
+function getData() {
+
+    // GET API DATA // limit=10, format=json, date=world ( dd/mm//yy )
+    let fearGreedUrl = "https://api.alternative.me/fng/?limit=10&format=json&date_format=world";
+    let fearGreedData;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            fearGreedData = JSON.parse(xhttp.responseText);
+            console.log(fearGreedData);
+            renderHTML(fearGreedData);
+        }
+    };
+    xhttp.open("GET", fearGreedUrl, true);
+    xhttp.send();
+    //
 }
 
-
-
-
-
+function renderHTML(fearGreedData) {
+    // BTC
+    let htmlString =
+    `
+    <div class="flex-item">Name: ${fearGreedData.name}</div>
+    <div class="flex-item">Value: ${fearGreedData.data[0].value}</div>
+    <div class="flex-item">Value classification: ${fearGreedData.data[0].value_classification}</div>
+    <div class="flex-item">timestamp: ${fearGreedData.data[0].timestamp}</div>
+    <div class="flex-item">time_until_update: ${fearGreedData.data[0].time_until_update}</div>
+    `
+    el.insertAdjacentHTML('beforeend', htmlString);
+}
